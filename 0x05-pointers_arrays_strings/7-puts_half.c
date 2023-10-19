7e04207cfb2ec107b2a554072c2ab3fa7141e821
SpanishFlu <saifeddinesayed@gmail.com>
2023-10-19 16:04:55 -0700

    a

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -11,8 +11,13 @@
 void puts_half(char *str)
 {
 	int l = 0;
-	
+
 	while (str[l] != '\0')
 		l++;
-	for (l /= 2; str[l] )
+	l++;
+	for (l /= 2; str[l] != '\0'; l++)
+	{
+		putchar(str[l]);
+	}
+	putchar('\n');
 }
