7ab937bd9ecb34d560e4e1fcf3f785ffd4aab4ff
SpanishFlu <saifeddinesayed@gmail.com>
2023-10-24 12:21:45 -0700

    asd

--- 0x06-pointers_arrays_strings/2-strncpy.c
+++ 0x06-pointers_arrays_strings/2-strncpy.c
@@ -15,7 +15,7 @@ char *_strncpy(char *dest, char *src, int n)
 	int i;
 	for (i = n; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
-	for (i = n; i < n && dest[i] != '\0'; i++)
+	for (i < n; i++)
 		dest[i] = '\0';
 	return (dest);
 }
