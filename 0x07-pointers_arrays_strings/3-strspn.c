89aafa263095145a44488a6f3e28dc93504bb8ca
SpanishFlu <saifeddinesayed@gmail.com>
2023-10-26 07:24:09 -0700

    sdad

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -2,38 +2,38 @@
 
 /**
  * _strspn - Gets the length of a prefix substring.
- * @s: The string to search in.
+ * @s: The string to search within.
  * @accept: The string containing characters to match.
  *
- * Return: The number of bytes in the initial segment of s consisting
- * only of bytes that appear in accept.
+ * Return: The number of bytes in the initial segment of s that consist
+ *         only of bytes from the accept string.
  */
 unsigned int _strspn(char *s, char *accept)
 {
 	unsigned int count = 0;
-	int found;
+	int i, j;
+	int match;
 
-	while (*s)
+	for (i = 0; s[i]; i++)
 	{
-		found = 0;
-		while (*accept)
+		match = 0;
+		for (j = 0; accept[j]; j++)
 		{
-			if (*s == *accept)
+			if (s[i] == accept[j])
 			{
-				count++;
-				found = 1;
+				match = 1;
 				break;
 			}
-			accept++;
 		}
-
-		if (found == 0)
+		if (match)
+		{
+			count++;
+		}
+		else
+		{
 			break;
-
-		s++;
-		accept = accept - count;
+		}
 	}
-
 	return (count);
 }
 
