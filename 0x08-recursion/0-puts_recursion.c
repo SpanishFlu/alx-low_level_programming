09c02a3617ffe81b8a57ba942e3931c54ab34427
SpanishFlu <saifeddinesayed@gmail.com>
2023-10-26 14:30:33 -0700

    ssads

--- 0x08-recursion/0-puts_recursion.c
+++ 0x08-recursion/0-puts_recursion.c
@@ -1,21 +1,17 @@
 #include "main.h"
 
 /**
- * _puts_recursion - prints a string, followed by a new line.
- *
- * @s: input string.
- *
- * Return: Always 0.
- */
-
+* _puts_recursion - Prints a string followed by a new line using recursion.
+* @s: The string to be printed.
+*/
 void _puts_recursion(char *s)
 {
-	int i = 0;
-
-	if (s[i] == '\0')
+	if (*s == '\0')  // Base case: if we reach the end of the string
 	{
-		putchar(0);
+		_putchar('\n'); // Print a newline character
+		return;
 	}
-	putchar(s[i]);
-	i++;
+	_putchar(*s); // Print the current character
+	_puts_recursion(s + 1); // Recursively call the function with the next character
 }
+
