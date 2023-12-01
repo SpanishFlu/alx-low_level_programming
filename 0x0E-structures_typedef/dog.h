16b001347c9245027ba3d8491f388d26555d3a2a
SpanishFlu <saifeddinesayed@gmail.com>
2023-12-01 12:03:46 -0800

    gddg

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,6 +1,11 @@
 #ifndef DOG_H
 #define DOG_H
-
+/**
+ * struct dog - Represents information about a dog
+ * @name: Name of the dog (char *)
+ * @age: Age of the dog (float)
+ * @owner: Owner of the dog (char *)
+ */
 struct dog
 {
 	char *name;
