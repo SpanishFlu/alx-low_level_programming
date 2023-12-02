d3267ad5419f58a940ed4a1bfe2664009ee12034
SpanishFlu <saifeddinesayed@gmail.com>
2023-12-02 14:59:59 -0800

    gffgf

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,11 @@
+#inlude "main.h"
+/**
+ * print_name - prints a name
+ * @name: name to be printed
+ * @f: function pointer that specifies the format
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	if (name != NULL && f != NULL)
+		f(name);
+}
