998fafab286bcbf84556024796c06872eeb4da02
SpanishFlu <saifeddinesayed@gmail.com>
2023-12-12 06:30:33 -0800

    gctdtd#

--- 0x13-more_singly_linked_lists/0-print_listint.c
+++ 0x13-more_singly_linked_lists/0-print_listint.c
@@ -11,10 +11,7 @@ size_t print_listint(const listint_t *h)
 
 	while (h != NULL)
 	{
-		if (h->n == NULL)
-			printf("%d\n", h->n);
-		else
-			printf("-> %lu elements\n", n);
+		printf("%d\n", h->n);
 		h = h->next;
 		non++;
 	}
