7b9fb1fd15b3f6d50222d45c2117753d43f4e9b7
SpanishFlu <saifeddinesayed@gmail.com>
2024-02-03 08:38:37 -0800

    awsddsf

---
+++ 0x0B-malloc_free/4-free_grid.c
@@ -0,0 +1,18 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - Frees a 2D grid created by alloc_grid.
+ * @grid: The 2D grid to be freed.
+ * @height: Height of the grid.
+ */
+void free_grid(int **grid, int height)
+{
+	int i;
+
+	if (gird == NULL || height <= 0)
+		return;
+	for (i = 0; i < height; i++)
+		free(grid(i));
+	free(grid);
+}
