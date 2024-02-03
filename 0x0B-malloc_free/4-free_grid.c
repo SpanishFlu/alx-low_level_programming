968121bd6c9420e924796a7c1c1d61f996f526dd
SpanishFlu <saifeddinesayed@gmail.com>
2024-02-03 08:41:59 -0800

    asdsds

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -10,9 +10,9 @@ void free_grid(int **grid, int height)
 {
 	int i;
 
-	if (gird == NULL || height <= 0)
+	if (grid == NULL || height <= 0)
 		return;
 	for (i = 0; i < height; i++)
-		free(grid(i));
+		free(grid[i]);
 	free(grid);
 }
