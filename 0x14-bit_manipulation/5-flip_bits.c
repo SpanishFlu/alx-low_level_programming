919acf365ca9edea9d19c5197089f0c036fb751e
SpanishFlu <saifeddinesayed@gmail.com>
2024-02-09 17:22:56 -0800

    asdsg

--- 0x14-bit_manipulation/5-flip_bits.c
+++ 0x14-bit_manipulation/5-flip_bits.c
@@ -12,7 +12,7 @@ unsigned int flip_bits(unsigned long int n, unsigned long int m)
 	unsigned long int XOR = n ^ m;
 	unsigned int count = 0;
 
-	if (XOR > 0)
+	while (XOR > 0)
 	{
 		count += XOR & 1;
 		XOR >>= 1;
