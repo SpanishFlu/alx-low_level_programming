3b8fec7f2d9e9b0a4a651bac7b3ac515d4b249e4
SpanishFlu <saifeddinesayed@gmail.com>
2024-02-16 00:14:13 -0800

    asdsfsf

--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -8,7 +8,74 @@
 #define BUFFER_SIZE 1024
 
 /**
- * main - Copies the content of a file to another file.
+ * open_file - Open a file and handle errors.
+ * @filename: The name of the file to open.
+ * @flags: Flags for the open syscall.
+ * @mode: Permissions for the file.
+ *
+ * Return: File descriptor on success, -1 on failure.
+ */
+int open_file(const char *filename, int flags, mode_t mode)
+{
+	int fd = open(filename, flags, mode);
+
+	if (fd == -1)
+	{
+		dprintf(2, "Error: Can't read from file %s\n", filename);
+		exit(98);
+	}
+	return (fd);
+}
+
+/**
+ * close_file - Close a file descriptor and handle errors.
+ * @fd: The file descriptor to close.
+ */
+void close_file(int fd)
+{
+	int close_status = close(fd);
+
+	if (close_status == -1)
+	{
+		dprintf(2, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * copy_file - Copy the content of one file to another.
+ * @fd_from: File descriptor of the source file.
+ * @fd_to: File descriptor of the destination file.
+ */
+void copy_file(int fd_from, int fd_to)
+{
+	ssize_t read_status, write_status;
+	char buffer[BUFFER_SIZE];
+
+	do {
+		read_status = read(fd_from, buffer, BUFFER_SIZE);
+		if (read_status == -1)
+		{
+			dprintf(2, "Error: Can't read from file\n");
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(98);
+		}
+
+		write_status = write(fd_to, buffer, read_status);
+		if (write_status == -1 || write_status != read_status)
+		{
+			dprintf(2, "Error: Can't write to file\n");
+			close_file(fd_from);
+			close_file(fd_to);
+			exit(99);
+		}
+
+	} while (read_status > 0);
+}
+
+/**
+ * main - Copy the content of a file to another file.
  * @argc: Number of arguments.
  * @argv: Array of arguments.
  *
@@ -16,8 +83,7 @@
  */
 int main(int argc, char *argv[])
 {
-	int fd_from, fd_to, read_status, write_status, close_status;
-	char buffer[BUFFER_SIZE];
+	int fd_from, fd_to;
 
 	if (argc != 3)
 	{
@@ -25,55 +91,18 @@ int main(int argc, char *argv[])
 		exit(97);
 	}
 
-	fd_from = open(argv[1], O_RDONLY);
-	if (fd_from == -1)
-	{
-		dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	fd_from = open_file(argv[1], O_RDONLY, 0);
+	fd_to = open_file
+		(argv[2],
+		 O_WRONLY | O_CREAT | O_TRUNC,
+		 S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH
+		 );
 
-	fd_to = open(argv[2], O_WRONLY | O_CREAT | O_TRUNC, S_IRUSR | S_IWUSR | S_IRGRP | S_IWGRP | S_IROTH);
-	if (fd_to == -1)
-	{
-		dprintf(2, "Error: Can't write to %s\n", argv[2]);
-		close(fd_from);
-		exit(99);
-	}
+	copy_file(fd_from, fd_to);
 
-	do
-	{
-		read_status = read(fd_from, buffer, BUFFER_SIZE);
-		if (read_status == -1)
-		{
-			dprintf(2, "Error: Can't read from file %s\n", argv[1]);
-			close(fd_from);
-			close(fd_to);
-			exit(98);
-		}
-
-		write_status = write(fd_to, buffer, read_status);
-		if (write_status == -1)
-		{
-			dprintf(2, "Error: Can't write to %s\n", argv[2]);
-			close(fd_from);
-			close(fd_to);
-			exit(99);
-		}
-	} while (read_status > 0);
-
-	close_status = close(fd_from);
-	if (close_status == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_from);
-		exit(100);
-	}
-	
-	close_status = close(fd_to);
-	if (close_status == -1)
-	{
-		dprintf(2, "Error: Can't close fd %d\n", fd_to);
-		exit(100);
-	}
+	close_file(fd_from);
+	close_file(fd_to);
 
 	return (0);
 }
+
